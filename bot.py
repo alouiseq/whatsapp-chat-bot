c97afa0fecb5b01e3b173a0ee624baff54fd341a
Louie <alouiseq@gmail.com>
2022-12-20 10:12:40 -0800

    add dog and joke

--- bot.py
+++ bot.py
@@ -20,11 +20,30 @@ def bot():
             quote = 'I could not retrieve a quote at this time, sorry.'
         msg.body(quote)
         responded = True
+    if 'joke' in incoming_msg:
+        r = requests.get('https://icanhazdadjoke.com', headers={'accept': 'application/json'}) 
+        if r.status_code == 200:
+            data = r.json()
+            joke = data['joke']
+        else:
+            joke = 'I could not retrieve a joke at this time, sorry.'
+        msg.body(joke)
+        responded = True
     if 'cat' in incoming_msg:
         msg.media('https://cataas.com/cat')
         responded = True
+    if 'dog' in incoming_msg:
+        r = requests.get('https://dog.ceo/api/breeds/image/random', headers={'accept': 'application/json'}) 
+        if r.status_code == 200:
+            data = r.json()
+            dog_url = data['message']
+            msg.media(dog_url)
+        else:
+            dog = 'I could not retrieve a dog at this time, sorry.'
+            msg.body(dog)
+        responded = True
     if not responded:
-        msg.body('I only know about famous quotes and cats, sorry!')
+        msg.body('I only know about famous quotes, cats, dogs, and joke, sorry!')
 
     return str(resp)
 
