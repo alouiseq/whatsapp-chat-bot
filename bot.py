c8719b96bad809597c5fc2862a4c9482bf554b7b
Louie <alouiseq@gmail.com>
2022-12-20 18:04:19 -0800

    keeping it dry

--- bot.py
+++ bot.py
@@ -4,46 +4,67 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
+failed_msg = 'Oh no! I can\'t find a {} for you at this time.'
+no_data_msg = 'Whoops, I seem to be missing that data! Ask me about famous quotes, cats, dogs, and jokes instead!'
+
+def getData(req_url, headers={'accept': 'application/json'}):
+    r = requests.get(req_url, headers=headers)
+    if r.status_code == 200:
+        return r.json()
+
 @app.route('/bot', methods=['POST'])
 def bot():
     incoming_msg = request.values.get('Body' '').lower()
     resp = MessagingResponse()
     msg = resp.message()
-    responded = False
+    searched = False
 
     if 'quote' in incoming_msg:
-        r = requests.get('https://api.quotable.io/random')
-        if r.status_code == 200:
-            data = r.json()
+        data = getData('https://api.quotable.io/random')
+        if data:
             quote = f'{data["content"]} ({data["author"]})'
         else:
-            quote = 'I could not retrieve a quote at this time, sorry.'
+            quote = failed_msg.format('quote')
+
         msg.body(quote)
-        responded = True
+        searched = True
     if 'joke' in incoming_msg:
-        r = requests.get('https://icanhazdadjoke.com', headers={'accept': 'application/json'}) 
-        if r.status_code == 200:
-            data = r.json()
+        data = getData('https://icanhazdadjoke.com')
+        if data:
             joke = data['joke']
         else:
-            joke = 'I could not retrieve a joke at this time, sorry.'
+            joke = failed_msg.format('joke')
         msg.body(joke)
-        responded = True
+        searched = True
     if 'cat' in incoming_msg:
         msg.media('https://cataas.com/cat')
-        responded = True
+        searched = True
     if 'dog' in incoming_msg:
-        r = requests.get('https://dog.ceo/api/breeds/image/random', headers={'accept': 'application/json'}) 
-        if r.status_code == 200:
-            data = r.json()
+        breed = None
+        data = getData('https://dog.ceo/api/breeds/list/all')
+        if data:
+            breeds = list(data['message'].keys())
+
+        for b in breeds:
+            if b in incoming_msg:
+                breed = b;
+                break
+
+        if breed:
+            data = getData(f'https://dog.ceo/api/breed/{breed}/images/random')
+        else:
+            data = getData('https://dog.ceo/api/breeds/image/random')
+
+        if data:
             dog_url = data['message']
             msg.media(dog_url)
         else:
-            dog = 'I could not retrieve a dog at this time, sorry.'
+            dog = failed_msg.format('dog')
             msg.body(dog)
-        responded = True
-    if not responded:
-        msg.body('I only know about famous quotes, cats, dogs, and joke, sorry!')
+
+        searched = True
+    if not searched:
+        msg.body(no_data_msg)
 
     return str(resp)
 
